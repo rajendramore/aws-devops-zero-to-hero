2625ecf3c5037c524d93999a2e30d830806af6bd
Rajendra More <99864837+rajendramore@users.noreply.github.com>
2025-05-19 00:01:54 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,6 +2,6 @@
 set -e
 
 # Stop the running container (if any)
-containerid=`docker ps |awk -F " " '{print $1}'`
-docker stop $containerid
-docker rm -f $containerid
+#containerid=`docker ps |awk -F " " '{print $1}'`
+docker stop python-flask
+docker rm -f python-flask
