a4087528a893d5f95a95d3e3809cfe907136c7a2
Rajendra More <99864837+rajendramore@users.noreply.github.com>
2025-05-19 01:15:52 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,5 +4,8 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull rajendramore84/simple-python-flask-app
 
+sudo docker stop python-flask
+sudo docker rm -f python-flask
+
 # Run the Docker image as a container
 docker run -d --name python-flask -p 5000:5000 rajendramore84/simple-python-flask-app
